4858ac8d330069c320a9673c73db7badd606e47e
Ramanjs <raman.s2662@gmail.com>
2023-06-04 20:52:11 +0530

    create fileUpdation model

---
+++ src/models/fileUpdation.ts
@@ -0,0 +1,53 @@
+import { DataTypes } from 'sequelize'
+import sequelize from '../databaseConnection'
+
+interface FileUpdationAttributes {
+  oldName: string
+  oldParent: string
+  newName: string
+  newParent: string
+  lastModified: Date
+  lastChanged: Date
+}
+
+const FileUpdation = sequelize.define('FileUpdation', {
+  oldName: {
+    type: DataTypes.STRING,
+    validate: {
+      min: 1,
+      max: 255
+    }
+  },
+  newName: {
+    type: DataTypes.STRING,
+    validate: {
+      min: 1,
+      max: 255
+    }
+  },
+  oldParent: {
+    type: DataTypes.STRING,
+    validate: {
+      min: 1,
+      max: 4096
+    }
+  },
+  newParent: {
+    type: DataTypes.STRING,
+    validate: {
+      min: 1,
+      max: 4096
+    }
+  },
+  lastModified: {
+    type: DataTypes.DATE,
+    allowNull: false
+  },
+  lastChanged: {
+    type: DataTypes.DATE,
+    allowNull: false
+  }
+})
+
+export type { FileUpdationAttributes }
+export default FileUpdation

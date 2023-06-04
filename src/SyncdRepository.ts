307dab5c6e3790a143775105e44c1ec1ac188cb6
Ramanjs <raman.s2662@gmail.com>
2023-06-05 01:59:55 +0530

    write pending changes to database

--- src/SyncdRepository.ts
+++ src/SyncdRepository.ts
@@ -3,7 +3,10 @@ import { type Model } from 'sequelize'
 import File, { type FileAttributes, type FileCreationAttributes } from './models/file'
 import Directory, { type DirectoryAttributes } from './models/directory'
 import type { FileUpdationAttributes } from './models/fileUpdation'
+import { statusConfig } from './config/status'
 import { lstat, opendir } from 'fs/promises'
+import sequelize from './databaseConnection'
+import FileUpdation from './models/fileUpdation'
 
 class SyncdRepository {
   workdir: string
@@ -12,9 +15,9 @@ class SyncdRepository {
   directories: Array<Model<DirectoryAttributes, DirectoryAttributes>>
   fileAdditions: FileCreationAttributes[]
   fileAdditionsMap: Map<string, FileCreationAttributes>
-  directoryAdditions: DirectoryAttributes[]
   fileDeletions: FileAttributes[]
   fileDeletionsMap: Map<string, FileAttributes>
+  directoryAdditions: DirectoryAttributes[]
   directoryDeletions: DirectoryAttributes[]
   fileUpdations: FileUpdationAttributes[]
 
@@ -56,10 +59,10 @@ class SyncdRepository {
     this.populateFileAdditionsMap()
     this.populateFileDeletionsMap()
 
-    for (const key in this.fileAdditionsMap.keys()) {
+    for (const [key, value] of this.fileAdditionsMap) {
       if (this.fileDeletionsMap.has(key)) {
         const oldFile = this.fileDeletionsMap.get(key)
-        const newFile = this.fileAdditionsMap.get(key)
+        const newFile = value
         if ((oldFile != null) && (newFile != null)) {
           this.fileUpdations.push({
             oldName: oldFile.name,
@@ -89,7 +92,8 @@ class SyncdRepository {
         path: directoryPath,
         lastModified: stat.mtime,
         lastChanged: stat.ctime,
-        parent: directoryParent
+        parent: directoryParent,
+        status: statusConfig.PENDING_ADDITION
       })
     } else {
       const oldDirectory = directory[0]
@@ -99,7 +103,8 @@ class SyncdRepository {
           path: directoryPath,
           lastModified: stat.mtime,
           lastChanged: stat.ctime,
-          parent: directoryParent
+          parent: directoryParent,
+          status: statusConfig.PENDING_ADDITION
         })
         this.directoryDeletions.push(oldDirectory.dataValues)
       }
@@ -122,7 +127,8 @@ class SyncdRepository {
         hash: '',
         lastModified: stat.mtime,
         lastChanged: stat.ctime,
-        parent: fileParent
+        parent: fileParent,
+        status: statusConfig.PENDING_ADDITION
       })
     } else {
       const oldFile = file[0]
@@ -133,7 +139,8 @@ class SyncdRepository {
           hash: '',
           lastModified: stat.mtime,
           lastChanged: stat.ctime,
-          parent: fileParent
+          parent: fileParent,
+          status: statusConfig.PENDING_ADDITION
         })
         this.fileDeletions.push(oldFile.dataValues)
       }
@@ -155,6 +162,54 @@ class SyncdRepository {
     } catch (err) {
       console.log(err)
     }
+  }
+
+  async setupDB (): Promise<void> {
+    const directories = await Directory.findAll()
+    if (directories.length === 0) {
+      const stat = await lstat('.')
+      await Directory.create({
+        path: '.',
+        lastModified: stat.mtime,
+        lastChanged: stat.ctime,
+        parent: '.',
+        status: statusConfig.DONE
+      })
+    }
+  }
+
+  async saveToDB (): Promise<void> {
+    this.filterUpdatedFiles()
+    await sequelize.sync()
+    await this.setupDB()
+    console.log(this.directoryAdditions)
+    await Directory.bulkCreate(this.directoryAdditions, {
+      validate: true
+    })
+    for (const directory of this.directoryDeletions) {
+      await Directory.update({
+        status: statusConfig.PENDING_DELETION
+      }, {
+        where: {
+          path: directory.path
+        }
+      })
+    }
+    await File.bulkCreate(Array.from(this.fileAdditionsMap.values()), {
+      validate: true
+    })
+    for (const file of this.fileDeletionsMap.values()) {
+      await File.update({
+        status: statusConfig.PENDING_DELETION
+      }, {
+        where: {
+          id: file.id
+        }
+      })
+    }
+    await FileUpdation.bulkCreate(this.fileUpdations, {
+      validate: true
+    })
   }
 }
 

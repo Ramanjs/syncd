0863ec3e348032392f20d82bf344edb150cfc936
Ramanjs <raman.s2662@gmail.com>
2023-06-04 12:03:48 +0530

    util functions to parallelly hash all file additions

---
+++ src/checksums.ts
@@ -0,0 +1,28 @@
+import os from 'os'
+import WorkerPool from './workerThreads/workerPool'
+import path from 'path'
+import { readFile } from 'fs/promises'
+import { type FileCreationAttributes } from './models/file'
+
+const pool = new WorkerPool(os.cpus().length, path.join(__dirname, 'workerThreads/worker.js'))
+
+async function hashFile (file: FileCreationAttributes): Promise<void> {
+  const fileContent = await readFile(file.path)
+  pool.runTask(fileContent, (err: Error | null, result: Uint8Array | null) => {
+    if (err != null) console.log(err)
+    if (result != null) file.hash = Buffer.from(result).toString('hex')
+    console.log(file)
+  })
+}
+
+function hashAllFiles (files: FileCreationAttributes[]): void {
+  pool.setNumFiles(files.length)
+  for (const file of files) {
+    void hashFile(file)
+  }
+}
+
+type hashCallback = (err: Error | null, result: Uint8Array | null) => void
+
+export type { hashCallback }
+export default hashAllFiles

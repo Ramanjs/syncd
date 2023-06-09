8cdc381559e661399512ad5a64437d4c0bca4668
Ramanjs <raman.s2662@gmail.com>
2023-06-09 16:33:31 +0530

    create init subcommand in commander

--- src/cli.ts
+++ src/cli.ts
@@ -1,31 +1,29 @@
-import { ArgumentParser } from 'argparse'
+import SyncdRepository from './SyncdRepository'
+import path from 'path'
+import authorize from './auth/authClient'
+import { google } from 'googleapis'
+import { Command } from 'commander'
+import { init } from './drive'
+const program = new Command()
 
-const parser = new ArgumentParser({
-  description: 'A CLI tool to sync your local directory with Google Drive'
-})
+program
+  .name('syncd')
+  .description('CLI to backup your local directory to Google Drive')
+  .version('0.1.0')
 
-const subparsers = parser.add_subparsers({
-  title: 'Commands',
-  dest: 'command',
-  required: true
-})
+program
+  .command('init')
+  .description('initialize an empty syncd repository on Drive')
+  .argument('<path-to-credentials>', 'path to your credentials file, must be in json format')
+  .argument('[path-to-directory]', 'path of directory you want to initialize', '.')
+  .action(async (pathToCredentials, pathToDirectory) => {
+    const repo = new SyncdRepository(pathToDirectory, true)
+    const sequelize = await repo.createRepo(pathToCredentials)
+    const credentialsPath = path.join(repo.syncddir, 'credentials.json')
+    const tokenPath = path.join(repo.syncddir, 'token.json')
+    const authClient = await authorize(credentialsPath, tokenPath)
+    const drive = google.drive({ version: 'v3', auth: authClient })
+    await init(sequelize, repo.worktree, drive)
+  })
 
-const argsp = subparsers.add_parser('init', {
-  help: 'Initialize a new, empty repository'
-})
-
-argsp.add_argument('path', {
-  metavar: 'directory',
-  nargs: '?',
-  default: '.',
-  help: 'where to create the repository'
-})
-
-function main (): void {
-  const args = parser.parse_args()
-  if (args.command === 'init') {
-    //
-  }
-}
-
-main()
+export default program

f832a31cc716cf6037f538ace8005e2fbbc4c2a6
Ramanjs <raman.s2662@gmail.com>
2023-06-09 16:39:27 +0530

    clean index script and call commander

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,5 @@
 #!/usr/bin/env node
 
-import SyncdRepository from './SyncdRepository'
-import hashAllFiles from './checksums'
-import sequelize from './databaseConnection'
-import { push } from './drive'
+import program from './cli'
 
-const repo = new SyncdRepository('.')
-
-async function main (): Promise<void> {
-  try {
-    await sequelize.sync()
-    await repo.loadDatabase()
-    await repo.walkWorkdir('.')
-    hashAllFiles(repo, push)
-  } catch (err) {
-    console.log(err)
-  }
-}
-
-void main()
+void program.parseAsync()
